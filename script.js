e384a12c302a423e8c4a8b52f50ad6510a5e2bb0
fauvel <gorm.t@orange.fr>
2020-08-24 17:42:03 +0200

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+// footer  1
+var mbappe = document.querySelector("footer");
+// footer  2
+for(let count = 1; 0 <= count; count++) {
+    mbappe.addEventListener('click',function(){
+    
+        console.log("click" + "numéro" + count);
+    });
+}
---
+++ scripts.js
@@ -0,0 +1,30 @@
+// footer  1
+var mbappe = document.querySelector("footer");
+mbappe.addEventListener('click',function(){
+    console.log("click " + "numéro " + count);
+})
+// footer  2
+var count = 1;
+mbappe.onclick = function() {
+  count += 1;
+  console.log("Click " + "numéro " + count);
+};
+
+
+//navbar
+var loulou = document.getElementsByTagName("span")[0];
+
+loulou.addEventListener('click',Mymy);
+
+function Mymy(){
+    var vasco = document.getElementById("navbarHeader");
+    vasco.classList.toggle("collapse");
+}
+//edit
+var sarace = document.getElementsByClassName("card mb-4 box-shadow")[0];
+
+var sa = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0];
+
+sa.addEventListener('click', function(){
+    sarace.style = "color: red";
+});
